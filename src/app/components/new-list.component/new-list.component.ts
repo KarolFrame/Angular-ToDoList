2103a866e67612dbf6e1cda8e1cd7d686b7699eb
Carolina <carolina.peseca@gmail.com>
2025-10-01 12:33:41 +0200

    new list component

---
+++ src/app/components/new-list.component/new-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-new-list.component',
+  imports: [CommonModule],
+  templateUrl: './new-list.component.html',
+  styleUrl: './new-list.component.css'
+})
+export class NewListComponent {
+
+}
